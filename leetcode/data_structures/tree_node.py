1c7c84d4cdf5472d11d7115f4b39d3c54fee850a
raymondshum <rshum@csumb.edu>
2022-04-26 18:04:33 -0700

    Added bfs print

--- leetcode/data_structures/tree_node.py
+++ leetcode/data_structures/tree_node.py
@@ -1,5 +1,23 @@
+from typing import List
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+    
+
+# From https://www.educative.io/edpresso/how-to-implement-a-breadth-first-search-in-python
+def bfs_print(visited: List[TreeNode], queue: List[TreeNode], graph: List[TreeNode], node: TreeNode):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        s = queue.pop(0) 
+        print (s, end = " ") 
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
