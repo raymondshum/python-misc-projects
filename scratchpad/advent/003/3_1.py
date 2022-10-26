302de3fcc9566e0b0b4e9730778d4c117610bbaf
raymondshum <rshum@csumb.edu>
2022-10-26 16:57:24 -0700

    Completed 3_2

--- scratchpad/advent/003/3_1.py
+++ scratchpad/advent/003/3_1.py
@@ -1,10 +1,15 @@
+from copy import deepcopy
 from scratchpad.advent.utils.utils import Utils
-from statistics import mode
+from statistics import mode, multimode
 from pprint import pprint
 
 def get_mode(input, index):
     column = [line[index] for line in input]
-    return mode(column)
+    modes = multimode(column)
+    if len(modes) == 1:
+        return modes[0]
+    else:
+        return '1'
 
 def invert_binary_string(binary_string):
     inverted_string = ""
@@ -40,6 +45,33 @@ def get_rate(input, rate):
 def get_power_consumption(epsilon, gamma):
     return epsilon * gamma
 
+def filter_list(input, rating_type):
+    result = deepcopy(input)
+    num_bits = len(input[0])
+    for position in range(num_bits):
+        mode = get_mode(result, position)
+        
+        if rating_type == 'oxygen':
+            bit_criteria = mode
+        else:
+            bit_criteria = invert_bit(mode)
+        
+        result = list(filter(
+            lambda string: 
+                int(string[position]) == int(bit_criteria),
+            result
+        ))
+
+        if len(result) == 1:
+            break
+        
+    return result
+    
+            
+
+def invert_bit(bit):
+    return '0' if bit == '1' else 1
+
 def main():
     file_path = r"input.txt"
     input = Utils.read_file(file_path) 
@@ -54,8 +86,21 @@ def main():
         rates[rate] = get_rate(input, rate)
         print(f"{rate}: {rates.get(rate)}")
     
-    print("power:", get_power_consumption(**rates))
+    print("power:", get_power_consumption(**rates), end="\n\n")
     
+    rating_types = {
+        'oxygen': 0,
+        'c02': 0
+    }
+    
+    for rating_type in rating_types:
+        result = filter_list(input,rating_type)
+        decimal = binary_to_decimal(result[0])
+        rating_types[rating_type] = decimal
+        print(f"{rating_type}: {decimal}")
+    
+    print("life support:", get_power_consumption(*rating_types.values()), end="\n\n")
+        
     
 
 if __name__ == '__main__':
